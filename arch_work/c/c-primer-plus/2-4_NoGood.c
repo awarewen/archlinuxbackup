ab9bc2a76eac41618e72bf84ea5376efb427efd1
GitNoWen <blacktonewascrazy@gmail.com>
2020-02-22 03:07:19 +0800

    add

---
+++ arch_work/c/c-primer-plus/2-4_NoGood.c
@@ -0,0 +1,19 @@
+//NoGood.c -- 有错误的程序
+#include <stdio.h>
+
+int main(void){
+
+
+	int n, n2, n3;
+
+	n = 5;
+	n2 = n * n;
+	n3 = n * n * n;
+
+	printf("n = %d , squared n2 = %d, cubed n3 = %d \n", n, n2, n3);
+
+
+	return 0;
+	
+	
+}
---
+++ arch_work/vedio/linux获取root权限09.md
@@ -0,0 +1,7 @@
+获取root权限
+
+更改root密码
+sudo password root
+
+su root 以root用户的身份打开一个终端
+
