2f02f233796a932d5c667140e05da5e1faa5fd99
GitNoWen <blacktonewascrazy@gmail.com>
2020-02-26 12:47:57 +0800

    /home/archwen/arch_work

---
+++ arch_work/c/c-primer-plus/5/5-1-shoes1.c
@@ -0,0 +1,23 @@
+// shoes.c -- 把鞋码转换成英寸
+//
+#include <stdio.h>
+
+#define ADJUST 7.31
+
+int main (void){
+
+
+  const double SCALE = 0.333;
+  double shoe, foot;
+
+
+  shoe = 9.0;
+  foot = SCALE * shoe + ADJUST;
+
+
+  printf("SHoe size (men's)  foot length\n");
+  printf("%10.1f %15.2finches\n", shoe, foot);
+
+
+  return 0;
+}
